d9e477b76f13d4bddedbf540ff8ec620eb229195
hubaidurrehman24 <135734408+hubaidurrehman24@users.noreply.github.com>
2024-03-28 21:10:02 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=144553;
+let accountEmail="hubaidurrehman03@gmail.com";
+var accountPassword="12345";
+accountCity="Karachi";
+ //accountId=2  //not allowed
+ accountEmail="111@gmail.com";
+ accountPassword="112233";
+ accountCity="multan";
+ 
+let accountstate;
+ console.log(accountId);
+ console.table([accountId,accountEmail,accountPassword,accountCity,accountstate]);
